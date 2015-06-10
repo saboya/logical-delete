58f1b693a545e06921492ad018edc93959dec5c9
Rodrigo Saboya <saboya@users.noreply.github.com>
2015-06-10 15:36:18 -0300

    whitespace

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,21 +6,21 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
 
-	inherits 'global'
-	log 'warn'
+    inherits 'global'
+    log 'warn'
 
-	repositories {
-		grailsCentral()
-		mavenLocal()
-		mavenCentral()
-	}
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+    }
 
-	dependencies {
-	}
+    dependencies {
+    }
 
-	plugins {
-		build ':release:3.0.1', ':rest-client-builder:1.0.3', {
-			export = false
-		}
-	}
+    plugins {
+        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
+          export = false
+        }
+    }
 }
