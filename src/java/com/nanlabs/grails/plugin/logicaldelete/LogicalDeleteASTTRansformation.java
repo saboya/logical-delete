db96ed8624ef703659cf9190bda56a0fc5811b1d
Rodrigo Saboya <saboya@users.noreply.github.com>
2015-06-11 03:03:28 +0100

    Reverting to static deleted property. Customizing this should be done by the user with the getter/setter methods.

--- src/java/com/nanlabs/grails/plugin/logicaldelete/LogicalDeleteASTTRansformation.java
+++ src/java/com/nanlabs/grails/plugin/logicaldelete/LogicalDeleteASTTRansformation.java
@@ -2,18 +2,20 @@ package com.nanlabs.grails.plugin.logicaldelete;
 
 import java.lang.reflect.Modifier;
 
-import org.codehaus.groovy.ast.*;
+import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.ast.ClassHelper;
+import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.expr.ConstantExpression;
-import org.codehaus.groovy.ast.stmt.ReturnStatement;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.grails.compiler.injection.GrailsASTUtils;
-import org.codehaus.groovy.transform.AbstractASTTransformation;
+import org.codehaus.groovy.transform.ASTTransformation;
 import org.codehaus.groovy.transform.GroovyASTTransformation;
 
 @GroovyASTTransformation(phase = CompilePhase.CANONICALIZATION)
-public class LogicalDeleteASTTRansformation extends AbstractASTTransformation {
+public class LogicalDeleteASTTRansformation implements ASTTransformation {
 
+    public final static String DELETED_FIELD_NAME = "deleted";
     public final static int CLASS_NODE_ORDER = 1;
 
     @Override
@@ -29,22 +31,10 @@ public class LogicalDeleteASTTRansformation extends AbstractASTTransformation {
     }
 
     private void addDeletedProperty(ClassNode node) {
-        String propertyName = getPropertyName(node);
-        if (!GrailsASTUtils.hasOrInheritsProperty(node, propertyName)) {
-            node.addProperty(propertyName, Modifier.PUBLIC, ClassHelper.boolean_TYPE, ConstantExpression.FALSE, null, null);
+        if (!GrailsASTUtils.hasOrInheritsProperty(node, DELETED_FIELD_NAME)) {
+            node.addProperty(DELETED_FIELD_NAME, Modifier.PUBLIC, ClassHelper.boolean_TYPE, ConstantExpression.FALSE, null, null);
         }
     }
-
-    private String getPropertyName(ClassNode node) {
-        AnnotationNode annotation = GrailsASTUtils.findAnnotation(node, LogicalDelete.class);
-        return getMemberStringValue(annotation,"property",getDefaultAnnotationArgumentValue(annotation));
-    }
-
-    private String getDefaultAnnotationArgumentValue(AnnotationNode annotation) {
-        ReturnStatement stmt = (ReturnStatement)annotation.getClassNode().getMethod("property", Parameter.EMPTY_ARRAY).getCode();
-        return (String)((ConstantExpression)stmt.getExpression()).getValue();
-    }
-
     private void implementDeletedDomainClassInterface(ClassNode node) {
         ClassNode iNode = new ClassNode(LogicalDeleteDomainClass.class);
         if (!iNode.implementsInterface(iNode)) {
