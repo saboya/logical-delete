c36107da6f9aed28152aa7f80fea5dbf8a24aeea
Rodrigo Saboya <saboya@users.noreply.github.com>
2015-06-12 19:18:31 -0300

    Trying coverall again.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,10 +14,18 @@ grails.project.dependency.resolution = {
         mavenLocal()
         mavenCentral()
     }
-
+    dependencies {
+        // Latest httpcore and httpmime for Coveralls plugin
+        build 'org.apache.httpcomponents:httpcore:4.3.2'
+        build 'org.apache.httpcomponents:httpclient:4.3.2'
+        build 'org.apache.httpcomponents:httpmime:4.3.3'
+    }
     plugins {
         build ':release:3.1.1', ':rest-client-builder:2.1.1', {
           export = false
+        }
+        test(':code-coverage:2.0.3-3') {
+            export = false
         }
     }
 }
