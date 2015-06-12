0120049e9aa69545c7dc4518e6b99d380aa7736f
saboya <saboya@gmail.com>
2015-06-12 15:48:24 -0300

    Organizing tests.

---
+++ test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/TestDomainCustomSpec.groovy
@@ -0,0 +1,38 @@
+package com.b2wdigital.grails.plugin.gormlogicaldelete
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+ */
+@TestFor(TestDomainCustom)
+class TestDomainCustomSpec extends Specification {
+
+    def setup() {
+        DomainClassEnhancer.enhance(grailsApplication.domainClasses)
+        grailsApplication.mainContext.addApplicationListener(new PreQueryListener())
+
+        3.times {
+            TestDomainCustom.findOrSaveByName("name" + it)
+        }
+    }
+
+    void "custom property domain"() {
+        given: "I have non-deleted domains"
+
+        when: "I delete one domain"
+        TestDomainCustom.first().delete()
+
+        then: "I should have X non-deleted domains, and X+1 domains overall"
+        TestDomainCustom.findAll().size() + 1 == withDeletedTestDomainCustomCount()
+    }
+
+    private int withDeletedTestDomainCustomCount() {
+        def count
+        TestDomainCustom.withDeleted {
+            count = TestDomainCustom.findAll().size()
+        }
+        return count
+    }
+}
---
+++ test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/TestDomainSpec.groovy
@@ -0,0 +1,47 @@
+package com.b2wdigital.grails.plugin.gormlogicaldelete
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(TestDomain)
+class TestDomainSpec extends Specification {
+
+    def setup() {
+        DomainClassEnhancer.enhance(grailsApplication.domainClasses)
+        grailsApplication.mainContext.addApplicationListener(new PreQueryListener())
+
+        3.times {
+            TestDomain.findOrSaveByName("name" + it)
+        }
+    }
+
+    void "saving annotated domain"() {
+        given: "I have 3 non-deleted domains"
+
+        when: "Save on a new domain is called"
+            TestDomain.findOrSaveByName("test name")
+
+        then: "I should have 4 non-deleted domains"
+             TestDomain.list().size() == 4
+            !TestDomain.findByName("test name").deleted == !false
+    }
+
+    void "deleting annotated domain"() {
+        given: "I have X non-deleted domains"
+
+        when: "I delete one domain"
+            TestDomain.first().delete()
+
+        then: "I should have X-1 non-deleted domains, and X domains overall"
+            TestDomain.findAll().size()+1 == withDeletedTestDomainCount()
+    }
+
+    private int withDeletedTestDomainCount() {
+        def count
+        TestDomain.withDeleted {
+             count = TestDomain.findAll().size()
+        }
+        return count
+    }
+}
