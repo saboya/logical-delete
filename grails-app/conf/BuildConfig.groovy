c940a8886dd1d0cee6bb5603543e462aed975426
Rodrigo Saboya <saboya@users.noreply.github.com>
2015-06-12 10:54:36 +0100

    Updating buildconfig.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,11 +15,8 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
 
-    dependencies {
-    }
-
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
+        build ':release:3.1.1', ':rest-client-builder:2.1.1', {
           export = false
         }
     }
