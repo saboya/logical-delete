892e6bb384751b93e894c32b186e59ae9c17ec16
saboya <saboya@gmail.com>
2015-06-13 00:58:53 +0100

    Using test domain class instead of having an ignored test just to declare a class for a test.

--- test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/GormLogicalDeleteASTTransformationTest.groovy
+++ test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/GormLogicalDeleteASTTransformationTest.groovy
@@ -7,10 +7,10 @@ import org.codehaus.groovy.tools.ast.TransformTestHelper
 class GormLogicalDeleteASTTransformationTest extends GrailsUnitTestCase {
 
     void test() {
-        def file = new File("test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/GormLogicalDeleteTest.groovy")
+        def file = new File("grails-app/domain/com/b2wdigital/grails/plugin/gormlogicaldelete/TestDomain.groovy")
         assert file.exists()
         def invoker = new TransformTestHelper(new GormLogicalDeleteASTTRansformation(), CompilePhase.CANONICALIZATION)
-        def clazz = invoker.parse(file)
+        Class<TestDomain> clazz = invoker.parse(file)
         def test = clazz.newInstance()
         test.deleted = true
         assert test.deleted
