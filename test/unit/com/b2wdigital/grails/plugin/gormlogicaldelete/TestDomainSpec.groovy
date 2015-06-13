6b6ad1164ca97fc8cf201c1f60051488240aeef1
Rodrigo Saboya <saboya@users.noreply.github.com>
2015-06-13 04:31:54 +0100

    Renaming tests, adding tests for physical delete and deleting with extra parameters.

--- test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/TestDomainSpec.groovy
+++ test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/TestDomainSpec.groovy
@@ -16,7 +16,7 @@ class TestDomainSpec extends Specification {
         }
     }
 
-    void "saving annotated domain"() {
+    void "saving default annotation domain and checking default deleted property state"() {
         given: "I have 3 non-deleted domains"
 
         when: "Save on a new domain is called"
@@ -27,7 +27,7 @@ class TestDomainSpec extends Specification {
             !TestDomain.findByName("test name").deleted == !false
     }
 
-    void "deleting annotated domain"() {
+    void "logically deleting default annotation domain"() {
         given: "I have X non-deleted domains"
 
         when: "I delete one domain"
@@ -35,6 +35,36 @@ class TestDomainSpec extends Specification {
 
         then: "I should have X-1 non-deleted domains, and X domains overall"
             TestDomain.findAll().size()+1 == withDeletedTestDomainCount()
+    }
+
+    void "logically deleting default annotation domain with extra parameter"() {
+        given: "I have X non-deleted domains"
+
+        when: "I delete one domain"
+        TestDomain.first().delete(flush: true)
+
+        then: "I should have X-1 non-deleted domains, and X domains overall"
+        TestDomain.findAll().size()+1 == withDeletedTestDomainCount()
+    }
+
+    void "phisically deleting default annotation domain"() {
+        given: "I have X non-deleted domains"
+
+        when: "I delete one domain"
+            TestDomain.first().delete(logicalDelete: false)
+
+        then: "I should have X-1 non-deleted domains, and X-1 domains overall"
+            TestDomain.findAll().size() == withDeletedTestDomainCount()
+    }
+
+    void "phisically deleting default annotation domain passing extra parameters"() {
+        given: "I have X non-deleted domains"
+
+        when: "I delete one domain"
+            TestDomain.first().delete(logicalDelete: false, flush: true)
+
+        then: "I should have X-1 non-deleted domains, and X-1 domains overall"
+            TestDomain.findAll().size() == withDeletedTestDomainCount()
     }
 
     private int withDeletedTestDomainCount() {
