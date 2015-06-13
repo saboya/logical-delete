0438fa5cea07a608ca760eb6199aa5e7c49a6aa9
saboya <saboya@gmail.com>
2015-06-13 03:12:00 +0100

    Backporting method to support Grails 2.3.

--- src/java/com/b2wdigital/grails/plugin/gormlogicaldelete/GormLogicalDeleteASTTRansformation.java
+++ src/java/com/b2wdigital/grails/plugin/gormlogicaldelete/GormLogicalDeleteASTTRansformation.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Modifier;
 
 import org.codehaus.groovy.ast.*;
 import org.codehaus.groovy.ast.expr.ConstantExpression;
+import org.codehaus.groovy.ast.expr.Expression;
 import org.codehaus.groovy.ast.stmt.ReturnStatement;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
@@ -44,7 +45,7 @@ public class GormLogicalDeleteASTTRansformation extends AbstractASTTransformatio
 
     private String getPropertyName(ClassNode node) {
         AnnotationNode annotation = GrailsASTUtils.findAnnotation(node, GormLogicalDelete.class);
-        return getMemberStringValue(annotation,"property",getDefaultAnnotationArgumentValue(annotation));
+        return userlandGetMemberStringValue(annotation, "property", getDefaultAnnotationArgumentValue(annotation));
     }
 
     private boolean getDeletedState(ClassNode node) {
@@ -53,6 +54,22 @@ public class GormLogicalDeleteASTTRansformation extends AbstractASTTransformatio
             return true;
         }
         return (Boolean)getMemberValue(annotation, "deletedState");
+    }
+
+    /**
+     * Backported to support Grails < 2.4
+     *
+     * @see <a href="https://github.com/groovy/groovy-core/commit/5f9797c00dbfcead70fdef85d35bb060f5e8caa1">5f9797c</a>
+     */
+    public static String userlandGetMemberStringValue(AnnotationNode node, String name, String defaultValue) {
+        final String STRING = "<DummyUndefinedMarkerString-DoNotUse>"; // from groovy.transform.Undefined
+        final Expression member = node.getMember(name);
+        if (member != null && member instanceof ConstantExpression) {
+            Object result = ((ConstantExpression) member).getValue();
+            if (result != null && result instanceof String && STRING.equals((String) result)) result = null;
+            if (result != null) return result.toString();
+        }
+        return defaultValue;
     }
 
     private String getDefaultAnnotationArgumentValue(AnnotationNode annotation) {
