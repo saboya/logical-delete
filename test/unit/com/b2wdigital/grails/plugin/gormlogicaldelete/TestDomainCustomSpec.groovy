a157ac894184809e6ee8914f30f42b756c181239
saboya <saboya@gmail.com>
2015-06-13 04:31:54 +0100

    Renaming tests, adding tests for physical delete and deleting with extra parameters.

--- test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/TestDomainCustomSpec.groovy
+++ test/unit/com/b2wdigital/grails/plugin/gormlogicaldelete/TestDomainCustomSpec.groovy
@@ -18,14 +18,55 @@ class TestDomainCustomSpec extends Specification {
         }
     }
 
-    void "custom property domain"() {
+    void "saving custom annotation domain and checking default deleted property state"() {
+        given: "I have 3 non-deleted domains"
+
+        when: "Save on a new domain is called"
+            TestDomainCustom.findOrSaveByName("test name")
+
+        then: "I should have 4 non-deleted domains"
+            TestDomainCustom.list().size() == 4
+            TestDomainCustom.findByName("test name").differentPropertyTest == true
+    }
+
+    void "logically deleting custom annotation domain"() {
         given: "I have non-deleted domains"
 
         when: "I delete one domain"
-        TestDomainCustom.first().delete()
+            TestDomainCustom.first().delete()
+
+        then: "I should have X non-deleted domains, and X+1 domains overall"
+            TestDomainCustom.findAll().size() + 1 == withDeletedTestDomainCustomCount()
+    }
+
+    void "logically deleting custom annotation domain with extra parameter"() {
+        given: "I have non-deleted domains"
+
+        when: "I delete one domain"
+        TestDomainCustom.first().delete(flush: true)
 
         then: "I should have X non-deleted domains, and X+1 domains overall"
         TestDomainCustom.findAll().size() + 1 == withDeletedTestDomainCustomCount()
+    }
+
+    void "phisically deleting custom annotation domain"() {
+        given: "I have X non-deleted domains"
+
+        when: "I delete one domain"
+            TestDomainCustom.first().delete(logicalDelete: false)
+
+        then: "I should have X-1 non-deleted domains, and X-1 domains overall"
+            TestDomainCustom.findAll().size() == withDeletedTestDomainCustomCount()
+    }
+
+    void "phisically deleting custom annotation domain with extra parameter"() {
+        given: "I have X non-deleted domains"
+
+        when: "I delete one domain"
+        TestDomainCustom.first().delete(logicalDelete: false, flush:true)
+
+        then: "I should have X-1 non-deleted domains, and X-1 domains overall"
+        TestDomainCustom.findAll().size() == withDeletedTestDomainCustomCount()
     }
 
     private int withDeletedTestDomainCustomCount() {
